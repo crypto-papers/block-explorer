9fd94c6830828dbba0a75582546f1c38c1631509
Marek Rewers <mrewers@users.noreply.github.com>
2019-10-20 23:55:18 -0400

    Tweak eslint and babel settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,34 @@
 module.exports = {
   extends: ['@cryptopapers/eslint-config', '@cryptopapers/eslint-config/react'],
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  rules: {
+    'import/no-unassigned-import': [
+      'error',
+      {
+        allow: ['**.css', '**.sass', '**/*.scss'],
+      },
+    ],
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['single', 'multiple', 'all', 'none'],
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx'],
+        paths: ['src'],
+      },
+    },
+  },
 };
