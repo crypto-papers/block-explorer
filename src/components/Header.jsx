408b0fd213dcc5468c1ba35d535266adaf554f7b
Marek Rewers <mrewers@users.noreply.github.com>
2019-10-21 03:12:03 -0400

    Style header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from '@reach/router';
 
+import '../style/header.scss';
+
 const Header = () => (
   <header>
-    <h1>Block Explorer</h1>
+    <h1 className="site-title">Block Explorer</h1>
     <nav>
       <Link to="/">Home</Link>
       <Link to="/block">Block</Link>
+      <Link to="/address">Search by Address</Link>
     </nav>
   </header>
 );
