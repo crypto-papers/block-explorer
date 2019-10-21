69c612d0c04f74ced9860b21432c2f4bff77e7ef
Marek Rewers <mrewers@users.noreply.github.com>
2019-10-21 03:15:58 -0400

    Add current block number to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import Web3 from 'web3';
 
-const web3 = new Web3(new Web3.providers.HttpProvider('http://gateway.rocketpool.net:8888'));
-
 const Home = () => {
-  const [blockIds, setBlockIds] = useState([]);
-  const [blockHashes, setBlockHashes] = useState([]);
+  // Const [blockIds, setBlockIds] = useState([]);
+  // const [blockHashes, setBlockHashes] = useState([]);
   const [currBlock, setCurrBlock] = useState(null);
+
+  const web3 = new Web3(new Web3.providers.HttpProvider('http://gateway.rocketpool.net:8888'));
 
   useEffect(() => {
     const getBlockNo = async () => {
-      const currBlockNo = await web3.eth.getBlockNumber().then(setCurrBlock);
-
-      return currBlockNo;
+      await web3.eth.getBlockNumber().then(setCurrBlock);
     };
 
     getBlockNo();
