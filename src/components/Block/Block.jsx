b6f2d8cf12cfe278d6349e73ea89c2f82d729ee1
Marek Rewers <mrewers@users.noreply.github.com>
2019-10-23 01:24:15 -0400

    Pull block data into block component

--- src/components/Block/Block.jsx
+++ src/components/Block/Block.jsx
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { string } from 'prop-types';
+
+import SearchBar from '../SearchBar/SearchBar';
+import provider from '../../utils/web3';
 
 import '../../style/globals.scss';
 
 const Block = ({ blockNum }) => {
+  const [block, setBlock] = useState(blockNum);
+  const [blockData, setBlockData] = useState({});
+
+  const getBlockInfo = async b => {
+    await provider.eth.getBlock(b, true).then(setBlockData);
+  };
+
+  useEffect(() => {
+    getBlockInfo(blockNum);
+  }, []);
+
   return (
-    <div className="container">
-      <h3 className="container-title">{`Block #${blockNum}`}</h3>
-    </div>
+    <Fragment>
+      <SearchBar
+        changeHandler={setBlock}
+        clickHandler={{ cb: getBlockInfo, params: [block] }}
+        label="Search by Block:"
+      />
+      <div className="container">
+        <h3 className="container-title">{`Block #${blockData.number || ''}`}</h3>
+        {blockData && (
+          <div>
+            <p>{`Timestamp: ${blockData.timestamp}`}</p>
+            <p>{`Block Hash: ${blockData.hash}`}</p>
+            <p>{`Block Size: ${blockData.size}`}</p>
+            <p>{`Gas Limit: ${blockData.gasLimit}`}</p>
+            <p>{`Gas Used: ${blockData.gasUsed}`}</p>
+            <p>{`Difficulty: ${blockData.difficulty}`}</p>
+            <p>{`Total Difficulty: ${blockData.totalDifficulty}`}</p>
+            <p>{`Nonce: ${blockData.nonce}`}</p>
+            <p>{`Parent Hash: ${blockData.parentHash}`}</p>
+            <p>{`SHA3 Uncles: ${blockData.sha3Uncles}`}</p>
+            <p>{`State Root: ${blockData.stateRoot}`}</p>
+            <p>{`Transactions Root: ${blockData.transactionsRoot}`}</p>
+            <p>{`Block Miner: ${blockData.miner}`}</p>
+            <p>{`Extra Data: ${blockData.extraData}`}</p>
+            {blockData.transactions &&
+              blockData.transactions.length > 0 &&
+              blockData.transactions.forEach(t => <p>{t}</p>)}
+          </div>
+        )}
+      </div>
+    </Fragment>
   );
 };
 
@@ -18,7 +60,7 @@ Block.propTypes = {
 };
 
 Block.defaultProps = {
-  blockNum: '0',
+  blockNum: 'latest',
 };
 
 export default Block;
