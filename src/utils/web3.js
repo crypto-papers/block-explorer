f8283c4c0e5e82df06d9fc3e1487ddf0370f22ba
Marek Rewers <mrewers@users.noreply.github.com>
2019-10-23 18:19:06 -0400

    Added rocketpool wrapper to web3 provider to allow interaction with Rocketpool chain

--- src/utils/web3.js
+++ src/utils/web3.js
@@ -1,5 +1,10 @@
 import Web3 from 'web3';
+import RocketPool from 'rocketpool';
 
-const provider = new Web3(new Web3.providers.HttpProvider('http://gateway.rocketpool.net:8888'));
+import RocketStorage from '../contracts/abi/RocketStorage.json';
 
-export default provider;
+export const provider = new Web3(
+  new Web3.providers.HttpProvider('http://gateway.rocketpool.net:8888')
+);
+
+export const rp = new RocketPool(provider, RocketStorage);
