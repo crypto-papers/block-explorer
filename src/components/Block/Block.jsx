3b979a364d7064a263121c5c7f1fb706c019c551
Marek Rewers <mrewers@users.noreply.github.com>
2019-10-25 00:13:13 -0400

    Style block component

--- src/components/Block/Block.jsx
+++ src/components/Block/Block.jsx
@@ -2,16 +2,25 @@ import React, { Fragment, useEffect, useState } from 'react';
 import { string } from 'prop-types';
 
 import SearchBar from '../SearchBar/SearchBar';
-import provider from '../../utils/web3';
+import { provider } from '../../utils/web3';
 
 import '../../style/globals.scss';
+import './Block.scss';
 
 const Block = ({ blockNum }) => {
   const [block, setBlock] = useState(blockNum);
-  const [blockData, setBlockData] = useState({});
+  const [blockData, setBlockData] = useState('');
+  const [latest, setLatest] = useState(false);
 
   const getBlockInfo = async b => {
     await provider.eth.getBlock(b, true).then(setBlockData);
+    const currentBlock = await provider.eth.getBlockNumber();
+
+    if (b === currentBlock || b === 'latest') {
+      setLatest(true);
+    } else {
+      setLatest(false);
+    }
   };
 
   useEffect(() => {
@@ -26,23 +35,51 @@ const Block = ({ blockNum }) => {
         label="Search by Block:"
       />
       <div className="container">
-        <h3 className="container-title">{`Block #${blockData.number || ''}`}</h3>
+        <h3 className="container-title">
+          {`Block #${blockData.number || ''}${latest ? ' - latest' : ''}`}
+        </h3>
         {blockData && (
-          <div>
-            <p>{`Timestamp: ${blockData.timestamp}`}</p>
-            <p>{`Block Hash: ${blockData.hash}`}</p>
-            <p>{`Block Size: ${blockData.size}`}</p>
-            <p>{`Gas Limit: ${blockData.gasLimit}`}</p>
-            <p>{`Gas Used: ${blockData.gasUsed}`}</p>
-            <p>{`Difficulty: ${blockData.difficulty}`}</p>
-            <p>{`Total Difficulty: ${blockData.totalDifficulty}`}</p>
-            <p>{`Nonce: ${blockData.nonce}`}</p>
-            <p>{`Parent Hash: ${blockData.parentHash}`}</p>
-            <p>{`SHA3 Uncles: ${blockData.sha3Uncles}`}</p>
-            <p>{`State Root: ${blockData.stateRoot}`}</p>
-            <p>{`Transactions Root: ${blockData.transactionsRoot}`}</p>
-            <p>{`Block Miner: ${blockData.miner}`}</p>
-            <p>{`Extra Data: ${blockData.extraData}`}</p>
+          <div className="grid">
+            <p className="grid-row">
+              <strong>Timestamp: </strong> {blockData.timestamp}
+            </p>
+            <p className="grid-row">
+              <strong>Block Hash: </strong> {blockData.hash}
+            </p>
+            <p className="grid-row">
+              <strong>Block Size: </strong> {blockData.size}
+            </p>
+            <p className="grid-row">
+              <strong>Gas Limit: </strong> {blockData.gasLimit}
+            </p>
+            <p className="grid-row">
+              <strong>Gas Used: </strong> {blockData.gasUsed}
+            </p>
+            <p className="grid-row">
+              <strong>Difficulty: </strong> {blockData.difficulty}
+            </p>
+            <p className="grid-row">
+              <strong>Total Difficulty: </strong> {blockData.totalDifficulty}
+            </p>
+            <p className="grid-row">
+              <strong>Nonce: </strong> {blockData.nonce}
+            </p>
+            <p className="grid-row">
+              <strong>Parent Hash: </strong> {blockData.parentHash}
+            </p>
+            <p className="grid-row">
+              <strong>SHA3 Uncles: </strong> {blockData.sha3Uncles}
+            </p>
+            <p className="grid-row">
+              <strong>State Root: </strong> {blockData.stateRoot}
+            </p>
+            <p className="grid-row">
+              <strong>Transactions Root: </strong> {blockData.transactionsRoot}
+            </p>
+            <p className="grid-row">
+              <strong>Block Miner: </strong> {blockData.miner}
+            </p>
+            {/* <p className="grid-row"><strong>Extra Data: </strong> {blockData.extraData}</p> */}
             {blockData.transactions &&
               blockData.transactions.length > 0 &&
               blockData.transactions.forEach(t => <p>{t}</p>)}
